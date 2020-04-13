c877cbd0161080fdd2524237bcf28f1b1b090db5
Ubuntu <vlassis.ld@gmail.com>
2020-04-13 18:06:36 +0000

    helper

--- helper.py
+++ helper.py
@@ -13,13 +13,6 @@
 # evt.cpu_time,  # CPU time avg
 # evt.cpu_time_total
 
-headers = [
-    'Name',
-    'CPU time avg (us)',
-    'CPU total (us)',
-    'Number of Calls'
-]
-
 def give_evt_lst(evt):
     return [evt.key, str(evt.cpu_time), 
             str(evt.cpu_time_total), str(evt.count)]
@@ -27,12 +20,36 @@ def give_evt_lst(evt):
 # give_evt_lst(prof.function_events[10])
 
 import csv
+from statistics import mean
 
 def save_to_csv(function_events, name):
-    with open('ptorch_csv/{}.csv'.format(name), mode='w') as file:
+    headers = [
+        'Name',
+        'CPU time avg (us)',
+        'CPU total (us)',
+        'Number of Calls'
+    ]
+    with open(name, mode='w') as file:
         writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
         #way to write to csv file
         writer.writerow(headers)
         for evt in function_events:
             writer.writerow( give_evt_lst(evt) )
+            
+def avg_2nodes(file1,file2,result_name):
+    from csvsort import csvsort
+    csvsort(file1, [0])
+    csvsort(file2, [0])
+    with open(file1) as csv1, open(file2) as csv2, open(result_name, mode='w') as csv_res:
+        read1 = csv.reader(csv1)
+        read2 = csv.reader(csv2)
+        writer = csv.writer(csv_res)
+        
+        headers = read1[0]
+        writer.writerow(headers)
+        for row1,row2 in zip(read1[1:],read2[1:]):
+            op = row1[0]
+            new_row = [ op ] + [ mean([x,y]) for x,y in zip(row1[1:],row2[1:]) ]
+            writer.writerow(new_row)
+            

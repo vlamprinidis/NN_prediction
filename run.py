3ce92b1029bec6bb6c80648d02d8740c414fcdf7
Ubuntu <vlassis.ld@gmail.com>
2020-08-04 21:49:07 +0000

    fix

--- run.py
+++ run.py
@@ -26,6 +26,6 @@ files = ['_alone.py', '_avg.py', '_conv.py', '_dense.py', '_drop.py', '_max.py',
 #TF
 cmd = TF(file = '_conv.py', 
          p1 = opt['all'](nf=32, b=32, nd=3, e=5, ch=3, dm=2),
-         p2 = opt['conv'](k=4, f=3, s=2))
+         p2 = opt['conv'](k=4, f=1, s=2))
 
 clean_go(cmd,3)
--- tensorflow/_conv.py
+++ tensorflow/_conv.py
@@ -46,6 +46,7 @@ if args.nodes > 1:
     else:
         workers = ["10.0.1.121:8890", "10.0.1.104:8890", "10.0.1.46:8890"]
     import json
+    import os
     os.environ['TF_CONFIG'] = json.dumps({
         'cluster': {
             'worker': workers

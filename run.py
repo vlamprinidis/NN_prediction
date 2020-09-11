d3952ccd33bc83a6d6e3a31cc8e412a190fb8903
Ubuntu <vlassis.ld@gmail.com>
2020-09-11 16:26:35 +0000

    stride-fix

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ for nodes in [3,2,1]:
         for batch in batch_ls:
             for channels in [1,3]:
                 for dim in [1,2]:
-                    for FRAME in [PT, TF]:
+                    for FRAME in [TF, PT]:
                         
                         opt_all = opt['all'](numf=numf, b=batch, nodes=nodes, e=epochs, ch=channels, dim=dim)
                         

01fb3fb69b07c1c69a81659131a99811b1e515bb
Vlassis Ld <vlassis.ld@gmail.com>
2020-09-26 19:34:08 +0300

    added new nets

--- tflow_models.py
+++ tflow_models.py
@@ -7,6 +7,37 @@ from tensorflow.keras.layers import Dense, Flatten
 opt = tf.keras.optimizers.SGD(learning_rate=0.01)
 loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
 
+# 28x28 input
+class LeNet1:        
+    def create(self):
+        model = Sequential()
+        model.add( 
+            layers.Conv2D(filters = 4, kernel_size = 5)
+        )
+        model.add(
+            layers.ReLU()       
+        )
+        model.add(
+            layers.MaxPooling2D(pool_size = 2, strides = 2)
+        )
+        model.add( 
+            layers.Conv2D(filters = 12, kernel_size = 5)
+        )
+        model.add(
+            layers.ReLU()        
+        )
+        model.add(
+            layers.MaxPooling2D(pool_size = 2, strides = 2)
+        )
+        model.add( Flatten() )
+        model.add( Dense(units = 10) )
+        
+        model.compile(loss = loss, optimizer = opt, metrics=['accuracy'])
+        
+        self.model = model
+        
+        return model
+    
 # 32x32 input
 class LeNet5:        
     def create(self):
@@ -49,4 +80,94 @@ class LeNet5:
         self.model = model
         
         return model
-                
+    
+# 32x32x3
+class VGG:
+    def create(self):
+        model = Sequential()
+        model.add(layers.Conv2D(32, (3, 3), strides = 1, padding = "same"))
+        model.add(layers.ReLU())
+        model.add(layers.Dropout(0.4))
+        
+        model.add(layers.Conv2D(32, (3, 3), strides = 1, padding = "same"))
+        model.add(layers.ReLU())
+        model.add(layers.Dropout(0.4))
+        
+        model.add(layers.MaxPooling2D((2, 2)))
+
+        model.add(layers.Conv2D(64, (3, 3), strides = 1, padding = "same"))
+        model.add(layers.ReLU())
+        model.add(layers.Dropout(0.4))
+        
+        model.add(layers.Conv2D(64, (3, 3), strides = 1, padding = "same"))
+        model.add(layers.ReLU())
+        model.add(layers.Dropout(0.4))
+        
+        model.add(layers.MaxPooling2D((2, 2)))
+
+        model.add(layers.Conv2D(128, (3, 3), strides = 1, padding = "same"))
+        model.add(layers.ReLU())
+        model.add(layers.Dropout(0.4))
+        
+        model.add(layers.Conv2D(128, (3, 3), strides = 1, padding = "same"))
+        model.add(layers.ReLU())
+        model.add(layers.Dropout(0.4))
+        
+        model.add(layers.MaxPooling2D((2, 2)))
+
+        model.add(Flatten())
+        model.add(Dense(10))
+        
+        self.model = model
+        
+        return model
+        
+# 227x227
+class AlexNet:
+    def create(self):
+        self.model = Sequential([
+            layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), input_shape=(227,227,3)),
+            layers.ReLU(),
+            layers.BatchNormalization(),
+            
+            layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+            
+            layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1)),
+            layers.ReLU(),
+            layers.BatchNormalization(),
+            
+            layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+            
+            layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1)),
+            layers.ReLU(),
+            layers.BatchNormalization(),
+            
+            layers.Conv2D(filters=384, kernel_size=(1,1), strides=(1,1)),
+            layers.ReLU(),
+            layers.BatchNormalization(),
+            
+            layers.Conv2D(filters=256, kernel_size=(1,1), strides=(1,1)),
+            layers.ReLU(),
+            layers.BatchNormalization(),
+            
+            layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+            
+            layers.Flatten(),
+            
+            layers.Dense(4096),
+            layers.ReLU(),
+            layers.Dropout(0.5),
+            
+            layers.Dense(4096),
+            layers.ReLU(),
+            layers.Dropout(0.5),
+            
+            layers.Dense(10)
+        ])
+        
+        return self.model
+        
+        
+        
+        
+
